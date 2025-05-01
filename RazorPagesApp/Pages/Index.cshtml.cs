0a098abc9cac060822d9a5169dc3392fdde1e4a9
jlbraga <jlbraga@fpf.br>
2025-05-01 18:25:44 -0400

    Final Commit of code

---
+++ RazorPagesApp/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace RazorPagesApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Nome { get; set; }
+        public List<Produto> Produtos { get; set; }
+
+        public IndexModel()
+        {
+            Produtos = new List<Produto>();
+            Nome = "Produto Default";
+        }
+
+        public void OnGet()
+        {
+            Produtos.Add(new Produto { Nome = "Produto 1", Preco = 10.0m });
+            Produtos.Add(new Produto { Nome = "Produto 2", Preco = 20.0m });
+            Produtos.Add(new Produto { Nome = "Produto 3", Preco = 30.0m });
+        }
+    }
+
+}
--- RazorPagesApp/obj/Debug/net9.0/RazorPagesApp.AssemblyInfo.cs
+++ RazorPagesApp/obj/Debug/net9.0/RazorPagesApp.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f1e832ab7577cac640014abdfeade2c360c711e")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyProductAttribute("RazorPagesApp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
